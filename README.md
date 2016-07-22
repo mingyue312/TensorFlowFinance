a10991cff6ed936cee4811fe3697190e13eb2640
LuyuanChen <claincly@hotmail.com>
2016-07-22 16:18:54 -0400

    Version 1
    - See detailed log in Readme.md

---
+++ README.md
@@ -0,0 +1,17 @@
+## 20160722 - Version 1
+
+### Run 0: 1 hidden layer with 10 units
+
+|        Feature       |      Figure      |
+|:--------------------:|:----------------:|
+|     Hidden layers    |         1        |
+| Hidden layer 1 units |        10        |
+|       Features       |         4        |
+|    Classifications   |         1        |
+|   Training set size  |        415       |
+|   Network mapping    |      Direct      |
+|  Hypothesis function |      Sigmoid     |
+|     Optimization     | Gradient Descent |
+|     Learning rate    |       0.005      |
+|      Iterations      |      110000      |
+|    Final accuracy    |       ~80%       |
--- cnn.py
+++ cnn.py
@@ -4,13 +4,14 @@ import pandas as pd
 from pandas_datareader import data
 import tensorflow as tf
 import numpy as np
+import sys
 
 # Define some macros
 TRAINING_SET_RATIO = 0.8
 
 #apple = Share('AAPL')
 #past_price = apple.get_historical('2016-05-01', '2016-05-31')
-training_test_data = data.DataReader('AAPL', 'yahoo', '2016-07-01')
+training_test_data = data.DataReader('AAPL', 'yahoo', '2014-07-01')
 
 training_test_data['Class'] = 0
 
@@ -53,39 +54,65 @@ sess = tf.Session()
 num_predictors = len(training_predictors_tf.columns)
 num_classes = 1
 
+# placeholders for feeding data
+# feature data size: BATCH_SIZE * num_predictors
 feature_data = tf.placeholder("float", [None, num_predictors])
-actual_classes = tf.placeholder("float", [None, 1])
+actual_classes = tf.placeholder("float", [None, num_classes])
 
-weights1 = tf.Variable(tf.truncated_normal([4, 20], stddev=0.0001))
-biases1 = tf.Variable(tf.ones([20]))
-weights2 = tf.Variable(tf.truncated_normal([20, 20], stddev=0.0001))
-biases2 = tf.Variable(tf.ones([20]))
-weights3 = tf.Variable(tf.truncated_normal([20, 1], stddev=0.0001))
-biases3 = tf.Variable(tf.ones([1]))
+# define weight matrices and assign some random values to it
+# for now, use a 1 hidden layer neural network
+# Input batch size  : 200 , TODO: now is whatever the input size is
+# hidden 1 units    : 50 , mapping size 50*200 + bias
+# output units      : 1  , mapping size  1* 50 + bias
+INPUT_BATCH_SIZE    = len(training_predictors_tf)
+HIDDEN_1_SIZE       = 10
+OUTPUT_SIZE         = num_classes
 
-hidden_layer_1 = tf.nn.relu(tf.matmul(feature_data, weights1) + biases1)
-hidden_layer_2 = tf.nn.relu(tf.matmul(hidden_layer_1, weights2) + biases2)
-model = tf.nn.softmax(tf.matmul(hidden_layer_2, weights3) + biases3)
+# input -> hidden layer
+weights1 = tf.Variable(tf.truncated_normal([num_predictors, HIDDEN_1_SIZE], stddev=0.0001))
+biases1 = tf.Variable(tf.ones([HIDDEN_1_SIZE]))
 
-cost = -tf.reduce_sum(actual_classes*tf.log(model))
+# hidden layer -> output
+weights2 = tf.Variable(tf.truncated_normal([HIDDEN_1_SIZE, OUTPUT_SIZE], stddev=0.0001))
+biases2 = tf.Variable(tf.ones([OUTPUT_SIZE]))
 
-train_op1 = tf.train.AdamOptimizer(learning_rate=0.00010).minimize(cost)
+# hidden layer 1 calculation
+hidden_layer_1 = tf.matmul(feature_data, weights1) + biases1
+model = tf.nn.sigmoid(tf.matmul(hidden_layer_1, weights2) + biases2)
+# hidden_layer_2 = tf.nn.relu(tf.matmul(hidden_layer_1, weights2) + biases2)
+# model = tf.nn.softmax(tf.matmul(hidden_layer_2, weights3) + biases3)
+
+cost = -tf.reduce_mean(actual_classes*tf.log(model) + (1-actual_classes)*tf.log(1-model))
+
+train_op1 = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
+
+# 0.01 learning rate
+optimizer = tf.train.GradientDescentOptimizer(0.005)
+train_op = optimizer.minimize(cost)
 
 init = tf.initialize_all_variables()
 sess.run(init)
 
-correct_prediction = tf.equal(tf.argmax(model, 1), tf.argmax(actual_classes, 1))
+# output: round the model
+output = tf.round(model)
+
+correct_prediction = tf.equal(output, actual_classes)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-for i in range(1, 30001):
+
+## this section optimizes the matrix for 30000 times and, use all training data at a time. Which, is super
+## computationally expensive
+##
+for i in range(1, 110001):
     sess.run(
-        train_op1,
+        train_op,
         feed_dict={
             feature_data: training_predictors_tf.values,
             actual_classes: training_classes_tf.values.reshape(len(training_classes_tf.values), 1)
         }
     )
-    if i%500 == 0:
+
+    if i%10000 == 0:
         print(i, sess.run(
             accuracy,
             feed_dict={
@@ -94,3 +121,7 @@ for i in range(1, 30001):
             }
         )
         )
+        # print(weights1.eval(session=sess))
+
+
+
